c8d514c98c8daa42f5ece388d60de5a65a810291
Alex Marandon <contact@alexmarandon.com>
2015-08-12 20:24:48 +0200

    Set up testing environment

---
+++ Makefile
@@ -0,0 +1,4 @@
+develop:
+	npm install
+test:
+	testem
---
+++ tests.js
@@ -0,0 +1,10 @@
+/* jshint mocha: true */
+/* global chai */
+var assert = chai.assert;
+
+describe('Hello', function() {
+  "use strict";
+  it('should be true', function() {
+    assert.equal(1, 1); 
+  });
+});
