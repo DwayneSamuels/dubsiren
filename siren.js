2623ed7e9026ee07d71ca6666102b582466585df
Alex Marandon <contact@alexmarandon.com>
2016-04-30 21:18:36 +0200

    Add volume meter

--- siren.js
+++ siren.js
@@ -12,10 +12,57 @@ var delay, feedback, filter,
     mainFrequencySlider = document.querySelector("input.mainFrequency"),
     modulationFrequencySlider = document.querySelector("input.modulationFrequency"),
     modulationAmplitudeSlider = document.querySelector("input.modulationAmplitude"),
-    outputVolumeSlider = document.querySelector("input.volume");
+    outputVolumeSlider = document.querySelector("input.volume"),
+    javascriptNode = ctx.createScriptProcessor(2048, 1, 1),
+    analyser = ctx.createAnalyser();
 
-outputGain.gain.value = outputVolumeSlider.value;
+
+(function initVolumeMeter() {
+    var canvasElement = document.getElementById("canvas");
+    var width = canvasElement.width, height = canvasElement.height;
+    var canvas = canvasElement.getContext("2d");
+    var gradient = canvas.createLinearGradient(0, 0, width, height);
+    gradient.addColorStop(1, '#da121a');
+    gradient.addColorStop(0.5, '#fcdd09');
+    gradient.addColorStop(0, '#078930');
+
+    javascriptNode.onaudioprocess = function() {
+        // get the average, bincount is fftsize / 2
+        var array = new Uint8Array(analyser.frequencyBinCount);
+        analyser.getByteFrequencyData(array);
+        var average = getAverageVolume(array);
+
+        // clear the current state
+        canvas.clearRect(0, 0, width, height);
+
+        // set the fill style
+        canvas.fillStyle = gradient;
+
+        // create the meters
+        canvas.fillRect(0, 0, (0 + average) * 1.8, height);
+    }
+}());
+
+
+function getAverageVolume(array) {
+    var values = 0;
+    var average;
+
+    var length = array.length;
+
+    for (var i = 0; i < length; i++) {
+        values += array[i];
+    }
+
+    average = values / length;
+    return average;
+}
+
+outputGain.gain.value = outputVolumeSlider.value / 2.0;
+javascriptNode.connect(ctx.destination);
 outputGain.connect(ctx.destination);
+outputGain.connect(analyser);
+analyser.connect(javascriptNode);
 
 function updateMainFrequency() {
     mainOscillator.frequency.value = mainFrequencySlider.value;
@@ -31,7 +78,7 @@ function updateModulationAmplitude() {
 
 
 outputVolumeSlider.addEventListener("input", function () {
-    outputGain.gain.value = outputVolumeSlider.value;
+    outputGain.gain.value = outputVolumeSlider.value / 2.0;
 });
 
 
